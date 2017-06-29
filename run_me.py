de8df93c3fdb487a987808aa04b36084bd34598f
Satyaki0924 <sanyal.satyaki09@gmail.com>
2017-06-29 12:25:46 +0530

    finalising file

--- run_me.py
+++ run_me.py
@@ -1,3 +1,11 @@
+"""
+This is a project by Satyaki Sanyal.
+This project must be used for educational purposes only.
+Follow me on:
+LinkedIn - https://www.linkedin.com/in/satyaki-sanyal-708424b7/
+Github - https://github.com/Satyaki0924/
+Researchgate - https://www.researchgate.net/profile/Satyaki_Sanyal
+"""
 from src.controller import Controller
 
 
